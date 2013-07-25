e6b58ff49234c0e8f875fe3b2a8be5c0bfb6002c
justzero <gb.justzero@gmail.com>
2013-07-25 15:31:33 +0800

    结构调整

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 chrome.app.runtime.onLaunched.addListener(function () {
     chrome.app.window.create("window.html", {bounds: {width:357, height:400}});
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+    console.log(request);
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.type !== "songtaste") return;
+
+    var mp3Url;
+    $.ajax({
+        url: request.url,
+        success: function (data) {
+            data.replace(/http:\/\/media.+\.mp3/g, function ($1) {
+                mp3Url = $1;
+                chrome.tabs.sendMessage(sender.tab.id, {type: "mp3Url", url: mp3Url}, function(response) {
+                });
+            });
+        }
+    });
+    sendResponse({farewell: 'loading ...'});
+});
+
